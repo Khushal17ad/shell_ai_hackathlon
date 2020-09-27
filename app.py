6bb45464efac1ea9542d4e90bf8a34f13b290902
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-09-27 17:43:52 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# import the required libraries
+import matplotlib.pyplot as plt  
+import numpy as np  
+import pandas as pd  
+import seaborn as sns  
+from sklearn import datasets  
+from sklearn import manifold  
+
+#%matplotlib inline  
+
+import streamlit as st
+import os
+
+def main():
+    #df = load_data()
+
+    st.header("Heroku App")
+
+
+    st.header("This is a test site")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# C:\Users\khush\github_projects\python_3.7\streamlit C:\Users\khush\github_projects\machine_learning\supervised_unsupervised_learning\unsupervised.py
+
+# In Anaconda Terminal - C:\Users\khush> streamlit run C:\Users\khush\github_projects\machine_learning\supervised_unsupervised_learning\unsupervised.py 
