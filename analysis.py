50ed9274caa6604bff1418aa69b8f9506861e705
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-09-27 18:25:57 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import plotly.express as px
 
 
 #cwd = os.getcwd()
@@ -8,4 +9,10 @@ import os
 def get_data():
 
     wind_2007 = pd.read_csv('./data/wind_data/wind_data_2007.csv')
-    return wind_2007
+
+    
+    fig = px.bar_polar(wind_20007, r="frequency", theta="drct",
+                   color="speed", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+
+    return fig
