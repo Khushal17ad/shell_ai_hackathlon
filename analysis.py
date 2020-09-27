2da279db7aa1dfbe48c2144fdd21e2109b8fd684
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-09-27 18:52:32 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -18,7 +18,7 @@ def get_rose_diagram(data):
 
     df = get_data()
     
-    fig = px.bar_polar(df, r="drct", theta="drct",
+    fig = px.bar_polar(df[:500], r="drct", theta="drct",
                    color="sped", template="plotly_dark",
                    color_discrete_sequence= px.colors.sequential.Plasma_r)
     
