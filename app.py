6ad753ee834fc10d416d743a2e7eeead5b2e2cef
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-10-02 19:33:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,3 +60,5 @@ if __name__ == "__main__":
 # C:\Users\khush\github_projects\python_3.7\streamlit C:\Users\khush\github_projects\machine_learning\supervised_unsupervised_learning\unsupervised.py
 
 # In Anaconda Terminal - C:\Users\khush> streamlit run C:\Users\khush\github_projects\machine_learning\supervised_unsupervised_learning\unsupervised.py 
+
+#https://www.ul.com/apps/wind-data-management-dashboard
