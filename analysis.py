588084d88c9d4b9283ec381eaf24d9a18846df1b
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-10-08 19:14:23 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -62,35 +62,6 @@ def get_time_plot(year):
     return fig
 
 
-def create_bins(lower_bound, width, quantity):
-    """ create_bins returns an equal-width (distance) partitioning. 
-        It returns an ascending list of tuples, representing the intervals.
-        A tuple bins[i], i.e. (bins[i][0], bins[i][1])  with i > 0 
-        and i < quantity, satisfies the following conditions:
-            (1) bins[i][0] + width == bins[i][1]
-            (2) bins[i-1][0] + width == bins[i][0] and
-                bins[i-1][1] + width == bins[i][1]
-    """
-    
-
-    bins = []
-    for low in range(lower_bound, 
-                     lower_bound + quantity*width + 1, width):
-        bins.append((low, low+width))
-    return bins
-
-def find_bin(value, bins):
-    """ bins is a list of tuples, like [(0,20), (20, 40), (40, 60)],
-        binning returns the smallest index i of bins so that
-        bin[i][0] <= value < bin[i][1]
-    """
-    
-    for i in range(0, len(bins)):
-        if bins[i][0] <= value < bins[i][1]:
-            return i
-    return -1
-
-
 def heat_map_analysis(year):
     file_name = 'wind_data_' + year + '.csv'
     data = get_data(file_name)
