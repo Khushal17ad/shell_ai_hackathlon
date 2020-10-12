92bf2a6137ea43156b951e6636b15afc0e7a9088
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-10-12 16:47:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,43 +25,47 @@ def main():
     year_list = os.listdir('./data/wind_data/')
     years = [y.split('_')[2].split('.csv')[0] for y in year_list]
     
-    #plot_option = st.sidebar.selectbox('Select the Plot',(' ', 'Rose Diagram'))
+    analysis_type = st.sidebar.selectbox('Select the Analysis Type',(' ', 'Single Year', 'Multi Year'))
 
-    #if plot_option == ' ':
-    #    print ('Please Select the Plot')
+    if analysis_type == ' ':
+        print ('Please Select the Analysis Type')
 
-    #elif plot_option == 'Rose Diagram':
+    elif plot_option == 'Rose Diagram':
 
     
 
-    year_option = st.sidebar.selectbox('Select the Year',(' ', '2007','2008','2009','2013','2014','2015','2017'))
+        year_option = st.sidebar.selectbox('Select the Year',(' ', '2007','2008','2009','2013','2014','2015','2017'))
 
-    #year_option = '2007'
-    if year_option == ' ':
-        print ('Please Select the Year')
-
-    else:
-        rose_diagram_header = 'Rose Diagram for the year: ' + year_option
-        st.subheader(rose_diagram_header)
-
-        dfe = analysis.analyze_rose_diagram_data(year_option)
-        st.plotly_chart(analysis.get_rose_diagram(dfe), width = 600, height = 600)
+        #year_option = '2007'
         
-        heat_diagram_header = 'Heat Map of Wind Speed and Direction for the year: ' + year_option
-        st.subheader(heat_diagram_header)
-        st.plotly_chart(analysis.heat_map_analysis(year_option), width = 1500, height = 800)
+        if year_option == ' ':
+            print ('Please Select the Year')
 
-        frequency_diagram_header = 'Histogram of Wind Speed for the year: ' + year_option
-        st.subheader(frequency_diagram_header)
-        st.plotly_chart(analysis.frequency_analysis(year_option), width = 1500, height = 600)
+        else:
+            rose_diagram_header = 'Rose Diagram for the year: ' + year_option
+            st.subheader(rose_diagram_header)
 
-        time_diagram_header = 'Time Series of for the year: ' + year_option
-        st.subheader(time_diagram_header)
-        st.plotly_chart(analysis.get_time_plot(year_option), width = 1500, height = 600)
+            dfe = analysis.analyze_rose_diagram_data(year_option)
+            st.plotly_chart(analysis.get_rose_diagram(dfe), width = 600, height = 600)
+            
+            heat_diagram_header = 'Heat Map of Wind Speed and Direction for the year: ' + year_option
+            st.subheader(heat_diagram_header)
+            st.plotly_chart(analysis.heat_map_analysis(year_option), width = 1500, height = 800)
 
-        #analysis.get_time_plot(year_option).update_layout(width = 900, height = 1500)
-        #analysis.get_time_plot(year_option).show()
-        #analysis.heat_map_analysis(year_option).show()
+            frequency_diagram_header = 'Histogram of Wind Speed for the year: ' + year_option
+            st.subheader(frequency_diagram_header)
+            st.plotly_chart(analysis.frequency_analysis(year_option), width = 1500, height = 600)
+
+            time_diagram_header = 'Time Series of for the year: ' + year_option
+            st.subheader(time_diagram_header)
+            st.plotly_chart(analysis.get_time_plot(year_option), width = 1500, height = 600)
+
+            #analysis.get_time_plot(year_option).update_layout(width = 900, height = 1500)
+            #analysis.get_time_plot(year_option).show()
+            #analysis.heat_map_analysis(year_option).show()
+    
+    else:
+        pass
 
 if __name__ == "__main__":
     main()
